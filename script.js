89347cabb60bbcf9b055ddab251f4dbe07175e2b
Your Name <you@example.com>
2020-10-24 17:30:20 +0500

    firs hw fin

---
+++ script.js
@@ -0,0 +1,67 @@
+
+for (let i = 1; i <=2 ; i++) {
+    var name1 = prompt('name please!')
+    var age1 = +prompt('age please!')
+    const users = {
+        i:{
+            name: name1,
+            age:  age1, 
+            
+        },
+        i:{
+            name: name1,
+            age:  age1, 
+            
+        },
+        i:{
+            name: name1,
+            age:  age1, 
+            
+        },
+        i:{
+            name: name1,
+            age:  age1, 
+            
+        },
+        i:{
+            name: name1,
+            age:  age1, 
+            
+        },
+        i:{
+            name: name1,
+            age:  age1, 
+            
+        },
+        i:{
+            name: name1,
+            age:  age1, 
+            
+        },
+        i:{
+            name: name1,
+            age:  age1, 
+            
+        },
+        i:{
+            name: name1,
+            age:  age1, 
+            
+        },
+        i:{
+            name: name1,
+            age:  age1, 
+            
+        },
+       
+    }
+    for(const key in users){
+        console.log(` user id #${i} ismi ${users[key].name} yoshi ${users[key].age}`);
+    }
+    // for(const main in users){
+    //     for(const least in users[main]){
+    //         console.log(`${i}-user is ${users[main].name} age ${users[main].age}`);
+    //     }
+    // }
+    
+}
